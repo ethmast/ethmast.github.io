56c0c4217d9df464de798494fff0ceb61984ee3f
ethmast <ethmast@gmail.com>
2024-10-12 16:17:15 -0400

    Add explanatory comments

--- javascript/home.js
+++ javascript/home.js
@@ -1,24 +1,24 @@
+// When user scrolls it calls the scrollRotate function.
 window.onscroll =  () => {
     scrollRotate();
 }
 
-let portrait = document.getElementById("rotate");
+let rotateElement = document.getElementById("rotate");
 
 
 scrollRotate = () => {
-//    console.log(portrait[0]);
-//    console.log(window.scrollY);
+    // Rotate text when user scrolls, up to a certain scroll point.
     if (window.scrollY < 110) {
-    portrait.style.transform = "rotate(-" + window.scrollY + "deg)";
+        rotateElement.style.transform = "rotate(-" + window.scrollY + "deg)";
     }
+    // When user scrolls past a certain point fade out the element.
     if (window.scrollY > 90) {
-        portrait.style.opacity = 0;
-        portrait.style.pointerEvents = 'all';
-    } else {
-        portrait.style.opacity = 1;
-        portrait.style.pointerEvents = 'none';
+        rotateElement.style.opacity = 0;
+        rotateElement.style.pointerEvents = 'all';
+    } else { // Fade back in if user scrolls back up.
+        rotateElement.style.opacity = 1;
+        rotateElement.style.pointerEvents = 'none';
     }
 }
 
 
-//console.log("rotate(" + window.scrollY/2 +"deg)")
