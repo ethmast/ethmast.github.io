49d48066ecc80be0c91ffd7778017a6d03f19d5e
ethmast <ethmast@gmail.com>
2025-05-25 14:40:35 -0400

    Refine the scroll animation of the portrait text element

--- javascript/home.js
+++ javascript/home.js
@@ -8,17 +8,18 @@ let rotateElement = document.getElementById("rotate");
 
 scrollRotate = () => {
     // Rotate text when user scrolls, up to a certain scroll point.
-    if (window.scrollY < 110) {
+    if (window.scrollY < 135) {
         rotateElement.style.transform = "rotate(-" + window.scrollY + "deg)";
     }
     // When user scrolls past a certain point fade out the element.
-    if (window.scrollY > 90) {
-        rotateElement.style.opacity = 0;
+    if (window.scrollY > 60 && window.scrollY < 130) {
+        rotateElement.style.opacity = (130 - window.scrollY) / 70;
         rotateElement.style.pointerEvents = 'all';
-    } else { // Fade back in if user scrolls back up.
-        rotateElement.style.opacity = 1;
-        rotateElement.style.pointerEvents = 'none';
-    }
+    } else if (window.scrollY > 130) {
+    rotateElement.style.opacity = 0;
+    } else if (window.scrollY < 60) {
+    rotateElement.style.opacity = 1;
+   }
 }
 
 
